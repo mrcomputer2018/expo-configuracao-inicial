7695b98ee719d0cdc724e9d5e45baf9f44ff660c
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-06-27 17:22:07 -0300

    add public stack navigation and entry screen component

--- src/navigations/public-stack.tsx
+++ src/navigations/public-stack.tsx
@@ -1,5 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import EntryScreen from "../screens/entry-screen";
+
 export default function PublicStack() {
+    const Stack = createNativeStackNavigator();
+
     return (
-        <></>
+        <Stack.Navigator>
+            <Stack.Screen 
+                name="Entry"
+                component={EntryScreen}
+                options={{ headerShown: false }}
+            />
+        </Stack.Navigator>
     )
 }
