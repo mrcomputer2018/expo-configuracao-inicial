a59e751121ca78a32c6c613ee9a5bd23829e1a60
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-06-28 08:16:55 -0300

    feat: implement AuthStack navigation and add HomeScreen component

--- src/navigation/auth-stack.tsx
+++ src/navigation/auth-stack.tsx
@@ -1,5 +1,15 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/home-screen";
+
 export default function AuthStack() {
+    const Stack = createNativeStackNavigator();
     return (
-        <></>
+        <Stack.Navigator>
+            <Stack.Screen 
+                name="Home"
+                component={HomeScreen}
+                options={{ headerShown: true }}
+            />
+        </Stack.Navigator>
     )
 }
